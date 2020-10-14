8c1a733e1bd23fd8268bf73d0fd6c59c5a43249a
jhou27 <72882154+jhou27@users.noreply.github.com>
2020-10-14 17:16:59 -0500

    Update Semantic_relatedness_qualtrics.js

--- test-task-audio/Semantic_relatedness_qualtrics.js
+++ test-task-audio/Semantic_relatedness_qualtrics.js
@@ -11,14 +11,20 @@ Qualtrics.SurveyEngine.addOnload(function () {
 
     /* Change 2: Defining and load required resources */
     // https://cdn.jsdelivr.net/gh/<github-username>/<repository-name>/<experiment-folder>
-    var task_github = "https://cdn.jsdelivr.net/gh/kywch/jsPsych-in-Qualtrics/flanker/";
+    var task_github = "http://jhou27.github.io/web-based-experiments/test-task-audio/";
 
     // requiredResources must include all the JS files that demo-simple-rt-task-transformed.html uses.
     var requiredResources = [
         task_github + "jspsych-6.1.0/jspsych.js",
         task_github + "jspsych-6.1.0/plugins/jspsych-html-keyboard-response.js",
-        task_github + "jspsych-6.1.0/plugins/jspsych-image-keyboard-response.js",
-        task_github + "flanker_main.js"
+        task_github + "jspsych-6.1.0/plugins/jspsych-html-button-response.js",
+        task_github + "jspsych-6.1.0/plugins/jspsych-audio-keyboard-response.js",
+        task_github + "jspsych-6.1.0/plugins/jspsych-instructions.js",
+        task_github + "js/practice_trial_info.js"
+        task_github + "js/amb_trial_info_v1.js"
+        task_github + "js/amb_trial_info_v2.js"
+        task_github + "filler_trial_info.js"
+        task_github + "Semantic_relatedness_main.js"
     ];
 
     function loadScript(idx) {
@@ -36,3 +42,47 @@ Qualtrics.SurveyEngine.addOnload(function () {
         loadScript(0);
     }
     
+    /* Change 3: Appending the display_stage Div using jQuery */
+    // jQuery is loaded in Qualtrics by default
+    jQuery("<div id = 'display_stage_background'></div>").appendTo('body');
+    jQuery("<div id = 'display_stage'></div>").appendTo('body');
+
+    /* Change 4: Wrapping jsPsych.init() in a function */
+    function initExp() {
+    
+    jsPsych.init({
+            timeline: timeline,
+            display_element: 'display_stage',
+            on_finish: function (data) {
+                /* Change 5: Summarizing and save the results to Qualtrics */
+                // summarize the results
+                var data_subset = jsPsych.data.get().filter({trial_part: 'probe', condition: condition});
+                var mean_rt = data_subset.select('rt').mean();
+                var acc = data_subset.filter({accuracy: true}).count() / data_subset.count();
+                
+                // save to qualtrics embedded data
+                Qualtrics.SurveyEngine.setEmbeddedData("acc", accuracy);
+                Qualtrics.SurveyEngine.setEmbeddedData("mean_rt", rt);
+                
+
+                /* Change 6: Adding the clean up and continue functions.*/
+                // clear the stage
+                jQuery('#display_stage').remove();
+                jQuery('#display_stage_background').remove();
+
+                // simulate click on Qualtrics "next" button, making use of the Qualtrics JS API
+                qthis.clickNextButton();
+            }
+        });
+    }
+});
+
+Qualtrics.SurveyEngine.addOnReady(function () {
+    /*Place your JavaScript here to run when the page is fully displayed*/
+
+});
+
+Qualtrics.SurveyEngine.addOnUnload(function () {
+    /*Place your JavaScript here to run when the page is unloaded*/
+
+});
