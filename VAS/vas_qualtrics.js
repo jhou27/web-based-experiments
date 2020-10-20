f4f43e496bc28abbcdcf8efd5dc95b73d243d6bf
jhou27 <72882154+jhou27@users.noreply.github.com>
2020-10-20 17:38:43 -0500

    Update vas_qualtrics.js

--- VAS/vas_qualtrics.js
+++ VAS/vas_qualtrics.js
@@ -10,14 +10,15 @@ Qualtrics.SurveyEngine.addOnload(function () {
     qthis.hideNextButton();
 
     /* Change 2: Defining and loading required resources */
-    var jslib_url = "https://kywch.github.io/jsPsych/";
+    var jslib_url = "http://jhou27.github.io/web-based-experiments/VAS/";
 
     // the below urls must be accessible with your browser
     // for example, https://kywch.github.io/jsPsych/jspsych.js
     var requiredResources = [
         'https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/4.0.30/Dropbox-sdk.min.js', // Change 6: Loading the Dropbox API
-        jslib_url + "jspsych.js",
-        jslib_url + "plugins/jspsych-html-keyboard-response.js"
+        jslib_url + "jspsych-6.1.0/jspsych.js",
+        jslib_url + "jspsych-6.1.0/plugins/jspsych-html-keyboard-response.js"
+        jslib_url + "VAS_main.js"
     ];
 
     function loadScript(idx) {
@@ -42,36 +43,36 @@ Qualtrics.SurveyEngine.addOnload(function () {
 
     /* Change 7: Adding necessary variables and functions for saving the results */
     // experimental session-defining variables
-    var task_name = "hello-world";
+    var task_name = "VAS";
     var sbj_id = "${e://Field/workerId}";
 
     // YOU MUST GET YOUR OWN DROPBOX ACCESS TOKEN
-    var dropbox_access_token = '<PUT YOUR Dropbox ACCESS TOKEN HERE>';
+    var dropbox_access_token = 'sl.Aj_WbPVJyAcljb3CeVTmukN5mg4eDQhOA2hcqp2a71EkxcKek6chyY3KKHcESFnIzEUD6-o0DUkt3gHAJjEw4H5Z1WuI7l1YZmYw-NewjkX9c_oSVL8_mnS2swRDfwgzzJYsXkw';
 
     // my preference is to include the task and sbj_id in the file name
     var save_filename = '/' + task_name + '/' + task_name + '_' + sbj_id;
 
-    function save_data_json() {
-        try {
-            var dbx = new Dropbox.Dropbox({
-                accessToken: dropbox_access_token
-            });
-            dbx.filesUpload({
-                    path: save_filename + '.json',
-                    mode: 'overwrite',
-                    mute: true,
-                    contents: jsPsych.data.get().json()
-                })
-                .then(function (response) {
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    console.error(error);
-                });
-        } catch (err) {
-            console.log("Save data function failed.", err);
-        }
-    }
+//     function save_data_json() {
+//         try {
+//             var dbx = new Dropbox.Dropbox({
+//                 accessToken: dropbox_access_token
+//             });
+//             dbx.filesUpload({
+//                     path: save_filename + '.json',
+//                     mode: 'overwrite',
+//                     mute: true,
+//                     contents: jsPsych.data.get().json()
+//                 })
+//                 .then(function (response) {
+//                     console.log(response);
+//                 })
+//                 .catch(function (error) {
+//                     console.error(error);
+//                 });
+//         } catch (err) {
+//             console.log("Save data function failed.", err);
+//         }
+//     }
 
     function save_data_csv() {
         try {
@@ -104,7 +105,7 @@ Qualtrics.SurveyEngine.addOnload(function () {
         }
 
         jsPsych.init({
-            timeline: [hello_trial],
+            timeline: timeline,
             display_element: 'display_stage',
 
             /* Change 5: Adding the clean up and continue functions.*/
@@ -115,7 +116,7 @@ Qualtrics.SurveyEngine.addOnload(function () {
                 // this must be done before saving
                 jsPsych.data.get().addToLast({participant: sbj_id});
 
-                save_data_json();
+               // save_data_json();
                 save_data_csv();
 
                 // clear the stage
